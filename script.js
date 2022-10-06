0bb588c14d06704a0df7db0c6184c0bdf47c854b
Rushikesh Patil <107334243+RushikeshPati20@users.noreply.github.com>
2022-10-06 22:04:13 +0530

    Dice game

---
+++ script.js
@@ -0,0 +1,27 @@
+
+// for img 1
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+var randomDiceimg = "dice" +randomNumber1+ ".png";
+var diceSrc ="images/"+ randomDiceimg;
+var image1 = document.querySelectorAll("img") [0];
+image1.setAttribute("src",diceSrc);
+
+
+// for img 2
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+var randomDiceimg2="dice"+randomNumber2+".png";
+var diceSrc2="images/"+randomDiceimg2;
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src",diceSrc2);
+
+// Who wins?
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML="  🚩Player 1 wins! ";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML=" 🚩Player 2 wins! ";
+}
+else {
+  document.querySelector("h1").innerHTML ="Draw!";
+}
